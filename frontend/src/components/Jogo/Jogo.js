c3fbec0c8feeca613c49fc38728d40084fc6cf95
LucasCruz <lca.snf21@uea.edu.br>
2022-08-22 23:52:45 -0400

    Player adicionado

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -1,10 +1,12 @@
 import "./Jogo.css";
 import clouds from "../../assets/clouds.png"
+import player from "../../assets/mario.gif"
 import pipe from "../../assets/pipe.png"
 
 function Jogo(){
     return <div className="jogo"> 
     <img className="nuvens" src={clouds} alt="nuvens"/>
+    <img className="player" src={player} alt="player"/>
     <img className="cano" src={pipe} alt="cano"/>
 
     </div>;

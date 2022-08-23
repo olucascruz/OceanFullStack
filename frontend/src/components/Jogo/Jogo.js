2b1cea02d2cfd3d14f9838e2338ab2a8a87eac7b
LucasCruz <lca.snf21@uea.edu.br>
2022-08-23 09:44:32 -0400

    Detecção de clique e criação de estado

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -2,12 +2,20 @@ import "./Jogo.css";
 import clouds from "../../assets/clouds.png"
 import player from "../../assets/mario.gif"
 import pipe from "../../assets/pipe.png"
+import {useState} from "react"
 
 function Jogo(){
+    //Estado 'estaPulando'
+    const [estaPulando, setEstaPulando] = useState(false);
+    
+    document.onkeydown = function(){
+        console.log("tecla");
+    };
+
     return <div className="jogo"> 
     <img className="nuvens" src={clouds} alt="nuvens"/>
 
-    <img className="player player-pulo" src={player} alt="player"/>
+    <img className="player" src={player} alt="player"/>
 
     <img className="cano" src={pipe} alt="cano"/>
 

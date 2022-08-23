514e5da9fe8b4d6c549b8f7da7ee3beb766806a7
LucasCruz <lca.snf21@uea.edu.br>
2022-08-23 10:31:43 -0400

    Pulo configurado

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -2,20 +2,31 @@ import "./Jogo.css";
 import clouds from "../../assets/clouds.png"
 import player from "../../assets/mario.gif"
 import pipe from "../../assets/pipe.png"
-import {useState} from "react"
+import React, {useState} from "react"
 
 function Jogo(){
+    console.log('telarenderizada');
     //Estado 'estaPulando'
     const [estaPulando, setEstaPulando] = useState(false);
-    
+    let playerClassName = "player";
+
     document.onkeydown = function(){
-        console.log("tecla");
+        if(!estaPulando){
+            setEstaPulando(true);
+        }    
     };
+    
+    if(estaPulando){
+        playerClassName = "player player-pulo";
+        setTimeout(()=>{
+            setEstaPulando(false);
+        }, 700);
+     };
 
     return <div className="jogo"> 
     <img className="nuvens" src={clouds} alt="nuvens"/>
 
-    <img className="player" src={player} alt="player"/>
+    <img className={playerClassName} src={player} alt="player"/>
 
     <img className="cano" src={pipe} alt="cano"/>
 

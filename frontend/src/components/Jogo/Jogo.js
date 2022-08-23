3a36c8c3841723a9e65be1297b378e2a9e9510f9
LucasCruz <lca.snf21@uea.edu.br>
2022-08-22 23:46:18 -0400

    Obstáculo adicionado

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -1,9 +1,11 @@
 import "./Jogo.css";
 import clouds from "../../assets/clouds.png"
+import pipe from "../../assets/pipe.png"
 
 function Jogo(){
     return <div className="jogo"> 
-    <img className="nuvens" src={clouds}/>
+    <img className="nuvens" src={clouds} alt="nuvens"/>
+    <img className="cano" src={pipe} alt="cano"/>
 
     </div>;
 }
